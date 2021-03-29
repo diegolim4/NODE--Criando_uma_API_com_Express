fb5defb83c718c7d2818f274606f4ec37dae9662
Suelen Almeida <lima.j.diego@gmail.com>
2021-03-29 17:07:44 -0300

    add link do servidor no app listen

--- index.js
+++ index.js
@@ -9,6 +9,4 @@ app.get("/page", (req, res) =>{
     res.sendFile(__dirname + '/html/page.html')
 })
 
-
-
-app.listen(31415, () => console.log('Servidor rodando...'))
+app.listen(31415, () => console.log('Servidor rodando no http://localhost:31415'))
