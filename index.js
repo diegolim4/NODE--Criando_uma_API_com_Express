e139f8d5fa453483cbdf0586d8673ff6f29cb73a
Suelen Almeida <lima.j.diego@gmail.com>
2021-03-29 15:50:46 -0300

    Adicionando rotas

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => { 
     res.send('Hello Word!')
+})
+
+app.get("/main", (req, res) =>{
+    res.send('Página dois!')
+})
+
+app.get("/page", (req, res) =>{
+    res.send('Página três')
 })
 
 app.listen(31415, () => console.log('Servidor rodando...'))
